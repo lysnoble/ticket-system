4ddd7a834539f8b336ef94a76e07910c2e00962c
alyssanoble <lysnoble@gmail.com>
2019-08-27 16:39:28 -0500

    IC-2018.2.4 <alyssanoble@alyssas-mbp Overwrite remote https://github.com/lysnoble/ticket-system to local

--- ticket-system/src/main/java/org/alyssanoble/models/data/MenuDao.java
+++
@@ -1,8 +0,0 @@
-package org.alyssanoble.models.data;
-
-
-import org.alyssanoble.models.Menu;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MenuDao extends CrudRepository<Menu, Integer> {
-}
